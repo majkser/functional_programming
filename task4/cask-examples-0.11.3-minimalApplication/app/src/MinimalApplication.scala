ecfb50a664731b77de41b114b30dd6bdfb338cb4
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-11-16 09:44:07 +0100

    feat: added append endpoint

--- task4/cask-examples-0.11.3-minimalApplication/app/src/MinimalApplication.scala
+++ task4/cask-examples-0.11.3-minimalApplication/app/src/MinimalApplication.scala
@@ -4,6 +4,18 @@ object MinimalApplication extends cask.MainRoutes:
   override def host = "0.0.0.0"
   override def port = 8080
 
+  @cask.postJson("/append")
+  def append(list: Option[Seq[Int]] = None, value: Option[Int] = None, index: Option[Int] = None) = {
+    val baseList = list.getOrElse(Seq.empty[Int])
+    val valueToInsert = value.getOrElse(0)
+    val indexValue = index.getOrElse(baseList.length)
+    
+    val (front, back) = baseList.splitAt(indexValue)
+    front ++ Seq(valueToInsert) ++ back
 
+    ujson.Obj(
+      "result" -> (front ++ Seq(valueToInsert) ++ back)
+    )
+  }
 
   initialize()
