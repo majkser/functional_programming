f0058b0eef2a157c3651a74e5a781e19f12d7ba4
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-11-30 12:29:10 +0100

    feat: implemented /count-nums

--- task5/cask-examples-0.11.3-minimalApplication/app/src/MinimalApplication.scala
+++ task5/cask-examples-0.11.3-minimalApplication/app/src/MinimalApplication.scala
@@ -4,4 +4,13 @@ object MinimalApplication extends cask.MainRoutes:
   override def host = "0.0.0.0"
   override def port = 8080
 
+  @cask.postJson("/count-nums")
+  def countNums(nums: Seq[Int]) = {
+    val map = nums.groupBy(identity).map((i, j) => (i, j.size))
+
+    ujson.Obj(
+      "count" -> map.map((i, j) => (i.toString, j)).toSeq
+    )
+  }
+
   initialize()
