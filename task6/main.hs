ab685c1d42b5f98ef3df3e4d7b97835d34813f63
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-12-21 23:14:19 +0100

    feat: completed part 1

--- task6/main.hs
+++ task6/main.hs
@@ -1,8 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import Data.Aeson(ToJSON, FromJSON, object, (.=))
+import Data.Text(Text)
+import GHC.Generics(Generic)
 
-main = scotty 3000 $
-  get "/:word" $ do
-    beam <- pathParam "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+data RequestBody = RequestBody
+  { values :: [Int]
+  } deriving (Show, Generic)
+
+instance FromJSON RequestBody
+instance ToJSON RequestBody
+
+isSorted :: [Int] -> Text -> Bool
+isSorted [] _ = True
+isSorted [_] _ = True
+isSorted lst order = case order of
+  "ascending"  -> and $ zipWith (<=) lst (tail lst)
+  "descending" -> and $ zipWith (>=) lst (tail lst)
+  _      -> False
+
+main :: IO ()
+main = scotty 3000 $ do
+
+  post "/isSorted/:order" $ do
+    body <- jsonData :: ActionM RequestBody
+    order <- param "order" :: ActionM Text
+    let sorted = isSorted (values body) order
+    json $ object ["are values sorted: " .= sorted]
