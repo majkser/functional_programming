163197319109f8334cffa4a8f021b65947a09750
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-12-24 12:12:32 +0100

    feat: completed part 1

--- task7/main.hs
+++ task7/main.hs
@@ -3,6 +3,33 @@
 import Web.Scotty
 import Data.Aeson(object, (.=))
 import Data.Text(Text)
+import Control.Monad.State
+import Data.IORef
+import Control.Monad.IO.Class (liftIO)
+
+nextRandomInt :: State Int Int
+nextRandomInt = do
+  currSeed <- Control.Monad.State.get
+  let
+    -- values taken from c++11's std::minstd_rand
+    a = 48271
+    c = 0
+    m = (2 ^ 31) - 1
+    nextSeed = (a * currSeed + c) `mod` m
+  Control.Monad.State.put nextSeed
+  return nextSeed
+
+bridge :: IORef s -> State s a -> IO a
+bridge ref stateAction = 
+  atomicModifyIORef ref $ \currentState ->
+    let (result, newState) = runState stateAction currentState
+    in (newState, result)
 
 main :: IO ()
-main = scotty 3000 $ do
+main = do
+  stateSeed <- newIORef 12345
+
+  scotty 3000 $ do
+    Web.Scotty.get "/random-int" $ do
+      randomInt <- liftIO $ bridge stateSeed nextRandomInt
+      json $ object ["random int: " .= randomInt]
