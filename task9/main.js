30ec6ffa9ed918c8f0716fc49c70f7136ca2964e
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2026-01-06 23:37:03 +0100

    feat: completed part1

--- task9/main.js
+++ task9/main.js
@@ -2,8 +2,33 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+Number.prototype.isPrime = function () {
+  if (this <= 1) return false;
+  if (this <= 3) return true;
+  for (let i = 2; i <= this - 1; i++) {
+    if (this % i === 0) return false;
+  }
+  return true;
+};
+
+app.post("/isPrime", (req, res) => {
+  let num = req.body.num;
+
+  if (typeof num !== "number") {
+    return res.status(400).json({ error: "Input must be a number" });
+  }
+
+  new Promise((resolve, reject) => {
+    if (num.isPrime()) {
+      resolve(1);
+    } else {
+      reject(0);
+    }
+  })
+    .then((result) => res.json({ "is prime: ": result }))
+    .catch((error) => res.json({ "is prime: ": error }));
 });
 
 app.listen(port, () => {
